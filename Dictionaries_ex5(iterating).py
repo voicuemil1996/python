22805007f815bc232b883c6a62b6e04b847ec066
Voicu <EVoicu@luxoft.com>
2020-08-27 14:30:12 +0300

    first commit

---
+++ Dictionaries_ex5(iterating).py
@@ -0,0 +1,10 @@
+dict = {'key1':'value1', 'key2':'value2'}
+
+for key in dict:
+
+    print(key, ':', dict[key])
+
+print('\n')
+for key, value in dict.items():
+
+    print(key, ':', value)
---
+++ Dictionaries_ex6.py
@@ -0,0 +1,9 @@
+n = 4
+dict = {}
+
+for i in range(1, n + 1):
+
+    dict[i] = i * i
+
+
+print(dict)
