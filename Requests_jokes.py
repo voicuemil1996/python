3f709154a300c62c548cfe72bc3dcd19924d9f7e
Voicu <EVoicu@luxoft.com>
2020-09-01 15:59:38 +0300

    request_jokes update

--- Requests_jokes.py
+++ Requests_jokes.py
@@ -1,9 +1,11 @@
 import requests
 
+
 listOfEndpoints = ['https://official-joke-api.appspot.com/random_joke', 'https://official-joke-api.appspot.com/jokes/random',
                    'https://official-joke-api.appspot.com/random_ten', 'https://official-joke-api.appspot.com/jokes/ten',
                    'https://official-joke-api.appspot.com/jokes/programming/random', 'https://official-joke-api.appspot.com/jokes/programming/ten']
 
+root = 'https://official-joke-api.appspot.com'
 #1. For each endpoint write a method that retrieves the information.
 #2. Format the output of the above mentioned methods in order to be more user friedly.
 #3. Retrieve 10 random jokes by type and for each joke verify if the type is correct. If not, raise an error.
@@ -11,40 +13,55 @@ listOfEndpoints = ['https://official-joke-api.appspot.com/random_joke', 'https:/
 
 
 #1
-def get_information(path = '/random_joke'):
+def get_information(path):
 
-    root = 'https://official-joke-api.appspot.com'
     endpoint = root + path
     response = requests.get(endpoint)
 
-    return response.json()
+    if type(response.json()) == list:
 
-#2
-def making_reading_friendly(path = '/random_joke'):
-
-    result = get_information(path)
-
-    if (type(result) == dict):
-
-        friendlyResult = result['setup'] + '\n ' + result['punchline']
+        return response.json()
 
     else:
 
-        friendlyResult = []
-        for dictionary in result:
-            friendlyResult.add(dictionary['setup'] + '\n ' + dictionary['punchline'])
+        return [response.json()]
 
-    return friendlyResult
+def get_friendly_information(path):
+    return making_reading_friendly(get_information(path))
+
+def get_random_joke():
+    return get_friendly_information('/random_joke')
+
+def get_random_ten():
+    return get_friendly_information('/random_ten')
+
+def get_random_ten_by_type():
+    return get_friendly_information('/jokes/programming/ten')
+
+def get_random_joke_by_type():
+    return get_friendly_information('/jokes/programming/joke')
+
+
+#2
+def making_reading_friendly(jokes):
+
+    friendlyJokes = []
+    for dictionary in jokes:
+        friendlyJokes.append(dictionary['setup'] + '\n ' + dictionary['punchline'])
+
+    return friendlyJokes
 
 #3
-def checking_type(path = '/jokes/programming/ten', type = 'programming'):
+def checking_type_for_10(type='programming'):
 
-    result = get_information(path)
+    path = '/jokes/programming/ten'
+    jokes = get_information(path)
 
     if type not in path:
-        print('Path must contain type!')
+        print('Type should be contained in path')
+        return False
 
-    for i in result:
+    for i in jokes:
         if i['type'] != type:
             print('There is a missmatch type!')
             return False
@@ -52,17 +69,22 @@ def checking_type(path = '/jokes/programming/ten', type = 'programming'):
     return True
 
 #4
-def displayng_odd_IDs(path = '/jokes/programming/ten'):
+def displayng_odd_IDs_for_10():
 
-    result = get_information(path)
+    jokes = get_information('/jokes/ten')
+    goodJokes = []
 
-    print('displayng jokes with odd ID')
-    for i in result:
+
+    for i in jokes:
         if int(i['id']) % 2 == 0:
-            print(i)
+            goodJokes.append(i)
 
-print("Test #1: ",get_information())
-print("Test #2: ",making_reading_friendly())
-print("Test #3: ",checking_type())
-print("Test #4: ",displayng_odd_IDs())
+    print('displayng jokes with odd ID\n')
+    for i in making_reading_friendly(goodJokes):
+        print(i)
+
+print("Test #1 and #2: ",get_random_ten(), "\n", get_random_joke())
+print("Test #3: ",checking_type_for_10())
+print("Test #4: ")
+displayng_odd_IDs_for_10()
 
