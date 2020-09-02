02edeeb4bb560ef7c6ba88fee6acb5ecb921d642
Voicu <EVoicu@luxoft.com>
2020-09-02 17:56:54 +0300

    update on AutomatedTestScript

--- AutomatedTestScript.py
+++ AutomatedTestScript.py
@@ -16,15 +16,16 @@ def displaying_most_recent_release():
     driver.get('https://www.python.org/');
     time.sleep(5) # Let the user actually see something!
 
-    downloads = driver.find_element_by_xpath("""//*[@id="downloads"]/a""")
-    allRelease = driver.find_element_by_xpath("""//*[@id="downloads"]/ul/li[1]/a""")
+    downloads = driver.find_element_by_xpath('.//li[@id="downloads"]/a')
+    allRelease = driver.find_element_by_xpath('//*[@id="downloads"]/ul/li[1]/a')
     ActionChains(driver).move_to_element(downloads).click(allRelease).perform()
-    driver.find_element_by_xpath("""//*[@id="content"]/div/section/div[2]/ol/li[1]/span[1]""").click()
 
-    time.sleep(5) # Let the user actually see something!
+    recentRelease = driver.find_element_by_xpath('.//div[@class="row download-list-widget"]//ol//li[1]//span[@class="release-number"]/a')
+    recentReleaseText = (str(recentRelease.text))
 
-    return True
+    return recentReleaseText
 
+displaying_most_recent_release()
 
 #	- Write an automated test script that:
 #		1. Opens a Chrome browser instance to the URL https://www.python.org/
@@ -35,10 +36,15 @@ def displaying_most_recent_release():
 
 def verify_example_count():
 
-    driver.get('https://www.python.org/');
+    driver.get('https://www.python.org/')
     time.sleep(5)  # Let the user actually see something!
 
-    driver.find_element_by_xpath("""//*[@id="id-search-field"]""").send_keys("decorator", Keys.ENTER)
+    driver.find_element_by_xpath('.//input[@id="id-search-field"]').send_keys("decorator", Keys.ENTER)
+    driver.find_element_by_xpath('.//ul[@class="list-recent-events menu"]//li[1]//h3//a').click()
+    driver.find_element_by_xpath('/html/body/div/div[3]/div/section/article/div[1]/ul/li[10]/a').click()
+
+    current_example_count = driver.find_element_by_xpath('/html/body/div/div[3]/div/section/article/div[1]/ul/li[10]/a')
     
+
 
 verify_example_count()
