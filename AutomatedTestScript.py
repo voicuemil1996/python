00d81891515728abd60fe287fb4ff2fbbe898f2b
Voicu <EVoicu@luxoft.com>
2020-09-02 10:46:18 +0300

    uploading second exercise

--- AutomatedTestScript.py
+++ AutomatedTestScript.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome("C:\\voicu emil\\chromedriver\\chromedriver.exe")
 
@@ -24,4 +25,20 @@ def displaying_most_recent_release():
 
     return True
 
-driver.quit()
+
+#	- Write an automated test script that:
+#		1. Opens a Chrome browser instance to the URL https://www.python.org/
+#		2. In Search bar input 'decorator' and search
+#		3. Open first Results link
+#       4. Select from Contents 'Examples'
+#		5. Verify current example count is 5
+
+def verify_example_count():
+
+    driver.get('https://www.python.org/');
+    time.sleep(5)  # Let the user actually see something!
+
+    driver.find_element_by_xpath("""//*[@id="id-search-field"]""").send_keys("decorator", Keys.ENTER)
+    
+
+verify_example_count()
